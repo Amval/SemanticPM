4b3477b5d3a6ec3cf87342487787b8eec6bc8c5c
Amval <alberto.m.valdunciel@gmail.com>
2016-06-14 19:17:25 +0200

    Domain Graph visualization in front-end

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -2,4 +2,8 @@ module CoursesHelper
   def has_file?(file)
     !file.url.nil?
   end
+
+  def graph_image_exists?(name)
+    FileTest.exist?("#{Rails.root}/public/img/graphs/#{name}.png")
+  end
 end
